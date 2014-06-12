115afda5409d280b39aa10636148c9d7dca97924
craigrow <craigrow@hotmail.com>
2014-06-12 16:22:38 -0700

    Basic functionality to pull quotes for my port
    x

--- getQuotes.py
+++ getQuotes.py
@@ -1,24 +1,20 @@
 from ystockquote import *
+import datetime
 
-f = open('d:\\git\\ystockquote\\testfile', 'r')
-stock = 'MSFT'
-print stock
-print(get_last_trade_price(stock))
+#Open a new file to write to and give it today's date as a filename.
+f = open("d:\\git\\ystockquote\\" + str(datetime.date.today()) + ".txt", 'w')
 
-stocks = 'MSFT', 'AMZN', 'UPL'
+#Create a list of stocks to get quote for.
+stocks = 'AIG', 'ALNY', 'AMBA', 'AMT', 'AMZN', 'BIDU', 'BOFI', 'BRK-B', 'CMG', 'DASTY', 'DDD', 'DNOW', 'GS', 'GTLS', 'INVN', 'IPGP', 'ISIS', 'ISRG', 'JNJ', 'JOBS', 'KMI', 'KO', 'LNKD', 'MA', 'MCD', 'MKL', 'NOV', 'OLED', 'P', 'PG', 'PRLB', 'PSMT', 'SSYS', 'TSLA', 'UPL', 'UPS', 'XONE', 'YNDX', 'Z'  
 
-
+#Loop through the list writing the latest price for each.
 for s in stocks:
     print s
-    print (get_last_trade_price(s))
-
-"""
-f = open('d:\\git\\ystockquote\\testfile', 'r')
-
-for line in f:
-    stock = str(line)
-    print stock
-    print(get_last_trade_price(stock))
-    print line
-    print price
-"""
+    quote = (get_last_trade_price(s))
+    print quote
+    string = str(s + ": " + quote + "\n")
+    print string
+    f.write(string)
+    
+#Close the file.    
+f.close()
